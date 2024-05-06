5bf1a3875110ec7124e56d4a5471ae1ba0d2991e
Alberto00093924 <164561461+Alberto00093924@users.noreply.github.com>
2024-05-06 12:34:29 -0600

    AYADIENDO EJERCICIO DE LA TABLA DEL 12

---
+++ ejercicio de la tabladel 12.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+using namespace std;
+int main()
+{
+    int i = 1;
+    while (i < 13)
+    {
+        cout << "12*"<< i<<" = "<< i * 12 << endl ;
+        i++;
+    }
+
+return 0;   
+}
+
+
